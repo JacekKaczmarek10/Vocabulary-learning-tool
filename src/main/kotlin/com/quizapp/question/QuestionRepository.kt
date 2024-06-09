19bd0a1fb120fbb25991cb455ef05f7e7818ccf7
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-09 17:51:54 +0200

    feat: add @Repository

--- src/main/kotlin/com/quizapp/question/QuestionRepository.kt
+++ src/main/kotlin/com/quizapp/question/QuestionRepository.kt
@@ -1,5 +1,7 @@
 package com.quizapp.question;
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface QuestionRepository : JpaRepository<Question, Long>
