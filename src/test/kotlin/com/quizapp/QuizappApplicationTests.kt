fc42d9763367b92f03c005f6b49b16511dd76b6f
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-09 17:53:01 +0200

    test: add tests

--- src/test/kotlin/com/quizapp/QuizappApplicationTests.kt
+++ src/test/kotlin/com/quizapp/QuizappApplicationTests.kt
@@ -1,13 +1,26 @@
 package com.quizapp
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 
 @SpringBootTest
 class QuizappApplicationTests {
 
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
     @Test
     fun contextLoads() {
+        assertThat(applicationContext).isNotNull
     }
 
+    @Test
+    fun main() {
+        val args = emptyArray<String>()
+        runApplication<QuizappApplication>(*args)
+    }
 }
